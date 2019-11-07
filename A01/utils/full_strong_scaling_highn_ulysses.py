13e469548a900cd64c1bea1b4764f7c3973a5f59
Emanuele Ballarin <emanuele@ballarin.cc>
2019-11-07 10:04:30 +0100

    Fix execution flags; add high-N strong scaling analysis

---
+++ A01/utils/full_strong_scaling_highn_ulysses.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from runacq import runacq  # Warning because it's a local file!
+
+# Loop variables
+averaging_range_len: int = 5
+proc_num: int = 20
+in_program_printed_values: int = 3
+
+# Test-specific variables
+proc_list = [2, 4, 8, 16, 20]
+niter_list = [5e7, 1e8, 5e8, 9e8, 1e9, 1.5e9, 2e9, 3e9]
+
+final_plot_matrix = []
+
+for niter in niter_list:
+
+    # Plotting-specific variables
+    outplot_gnutime = []  # To be filled
+
+    # Varying number of processors
+    for proc in proc_list:
+
+        # Averaging loop
+        gnutime_accumulator = 0
+        for trial in range(averaging_range_len):
+
+            # Actual execution
+            current_run = runacq("../src/a.out", proc, niter, isserial=False)
+            gnutime_accumulator += float(current_run[1][0])
+
+        # Actual averaging
+        gnutime_average = float(gnutime_accumulator) / float(averaging_range_len)
+        outplot_gnutime.append(gnutime_average)
+
+    final_plot_matrix.append(outplot_gnutime)
+
+print(final_plot_matrix)
