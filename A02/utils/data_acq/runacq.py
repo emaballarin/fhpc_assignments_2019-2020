20f82e5cbe20c8c49f10e74116a27ff4b54a1d07
Emanuele Ballarin <emanuele@ballarin.cc>
2019-12-16 02:55:26 +0100

    Some data acquisition machinery

---
+++ A02/utils/data_acq/runacq.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+#
+# (C) 2019-* Emanuele Ballarin <emanuele@ballarin.cc>
+# Distribution: MIT License.
+
+# IMPORTS:
+import os
+import subprocess as proc
+from subprocess import PIPE
+import time
+import numpy as np
+
+
+###################################################################################################
+
+
+def esf(speedup_pass, nthreads_pass):
+    """Empirical Serial Fraction, given parallel speedup and number of threads
+    (after [Tornatore, 2019])"""
+
+    return ((1.0 / float(speedup_pass)) - (1.0 / float(nthreads_pass))) / (
+        1.0 - (1.0 / float(nthreads_pass))
+    )
+
+
+###################################################################################################
+
+
+def runacq_touchfirst(bin_name, nthreads, vector_len, isserial=False):
+
+    bin_path = "../../src/touch_by/"
+
+    call_env = os.environ.copy()
+    call_env["OMP_NUM_THREADS"] = str(nthreads)
+
+    execlist = [str(bin_path + bin_name), str(vector_len)]
+    ranproc = proc.run(execlist, stdout=PIPE, stderr=PIPE, check=True, env=call_env)
+
+    ranproc_stout = str(ranproc.stdout.decode())
+    ranproc_linesout = list(filter(None, ranproc_stout.split("\n")))
+    print(ranproc_linesout)
+    clean_linesout = [ranproc_linesout[-3], ranproc_linesout[-2], ranproc_linesout[-1]]
+    timesout = [
+        float(clean_linesout[0]),  # Wall clock time
+        float(clean_linesout[1]),  # Average single thread time
+        float(clean_linesout[2]),  # Minimum single thread time
+    ]
+
+    if isserial:
+        timesout = [
+            timesout[0]
+        ]  # Thread times are undefined and thus contain garbage values!
+
+    time.sleep(1)
+    return timesout  # LIST: [walltime, threadavg, threadmin]
+
+
+###################################################################################################
+
+
+def runacq_touchbyall(bin_name, nthreads, vector_len, isserial=None):
+
+    bin_path = "../../src/touch_by/"
+
+    call_env = os.environ.copy()
+    call_env["OMP_NUM_THREADS"] = str(nthreads)
+
+    execlist = [str(bin_path + bin_name), str(vector_len)]
+    ranproc = proc.run(execlist, stdout=PIPE, stderr=PIPE, check=True, env=call_env)
+
+    ranproc_stout = str(ranproc.stdout.decode())
+    ranproc_linesout = list(filter(None, ranproc_stout.split("\n")))
+    clean_linesout = [ranproc_linesout[-1]]
+    timesout = [
+        float(clean_linesout[0]),  # Wall clock time
+    ]
+
+    time.sleep(1)
+    return timesout  # LIST: [walltime]
+
+
+###################################################################################################
+
+
+def runacq_prefixsum(bin_name, nthreads, vector_len, isserial=None):
+
+    bin_path = "../../src/prefix_sum/"
+
+    call_env = os.environ.copy()
+    call_env["OMP_NUM_THREADS"] = str(nthreads)
+
+    execlist = [str(bin_path + bin_name), str(vector_len)]
+    ranproc = proc.run(execlist, stdout=PIPE, stderr=PIPE, check=True, env=call_env)
+
+    ranproc_stout = str(ranproc.stdout.decode())
+    ranproc_linesout = list(filter(None, ranproc_stout.split("\n")))
+    clean_linesout = [ranproc_linesout[-2], ranproc_linesout[-1]]
+
+    timesout = [
+        float(clean_linesout[0]),  # Serial runtime - in nanoseconds
+        float(clean_linesout[1]),  # Parallel runtime - in nanoseconds
+        float(0.0),  # Speedup (still to be computed)
+    ]
+
+    timesout = [
+        timesout[0] / 10e9,  # Serial runtime - in seconds
+        timesout[1] / 10e9,  # Parallel runtime - in seconds
+        timesout[0] / timesout[1],  # Speedup
+        # Empirical serial fraction
+    ]
+
+    timesout = [
+        timesout[0],  # Serial runtime - in seconds
+        timesout[1],  # Parallel runtime - in seconds
+        timesout[2],  # Speedup
+        (
+            esf(timesout[2], nthreads)  # Empirical serial fraction
+            #            ((1.0 / float(timesout[2])) - (1.0 / float(nthreads)))
+            #            / (1.0 - (1.0 / float(nthreads)))
+        ),
+    ]
+
+    time.sleep(1)
+    return timesout  # LIST: [serialtime, paralleltime, speedup]
+
+
+###################################################################################################
+
+
+def acqavg(nrtimes, funcname, bin_name, nthreads, vector_len, isserial_pass=None):
+    """Iterate the acquisition of data and average the results"""
+
+    if (funcname == "runacq_touchfirst") and (isserial_pass is None):
+        raise Exception(
+            "acqavg: function 'runacq_touchfirst' called with invalid 'isserial=None' argument!"
+        )
+
+    # First, out-of-loop call
+    ret_result = funcname(bin_name, nthreads, vector_len, isserial=isserial_pass)
+    accumulator_vector = np.asarray(ret_result, float)
+
+    for i in range(nrtimes - 1):
+        ret_result = funcname(bin_name, nthreads, vector_len, isserial=isserial_pass)
+        accumulator_vector += np.asarray(ret_result)
+
+    mean = accumulator_vector / nrtimes
+
+    return list(mean)
+
+
+###################################################################################################
---
+++ A02/utils/data_acq/scaling.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+#
+# (C) 2019-* Emanuele Ballarin <emanuele@ballarin.cc>
+# Distribution: MIT License.
+
+# IMPORTS:
+from runacq import *
+
+# GLOBALS
+procnum_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+averaging_num = 5
+probsize = 3000000000  # Do not use scientific notation
+
+
+##################################################################################################
+
+
+# Strong scaling of "Touch First"
+
+tf_xaxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+tf_serial_run = acqavg(
+    averaging_num, runacq_touchfirst, "tf_serial.x", 1, probsize, True
+)
+tf_serial_time = tf_serial_run[0]
+
+tf_parallel_time = []
+tf_parallel_overh = []
+for proc in procnum_list:
+    procrun = acqavg(
+        averaging_num, runacq_touchfirst, "tf_parallel.x", proc, probsize, False
+    )
+    tf_parallel_time.append(procrun[0])
+    tf_parallel_overh.append(esf(float(tf_serial_time / procrun[0]), proc))
+
+
+##################################################################################################
+
+
+# Strong scaling of "Touch By All"
+
+tba_xaxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+tba_serial_run = acqavg(
+    averaging_num, runacq_touchbyall, "tba_serial.x", 1, probsize, True
+)
+tba_serial_time = tba_serial_run[0]
+
+tba_parallel_time = []
+tba_parallel_overh = []
+for proc in procnum_list:
+    procrun = acqavg(
+        averaging_num, runacq_touchbyall, "tba_parallel.x", proc, probsize, False
+    )
+    tba_parallel_time.append(procrun[0])
+    tba_parallel_overh.append(esf(float(tba_serial_time / procrun[0]), proc))
+
+
+##################################################################################################
+
+
+# Strong scaling of "Prefix Sum"
+
+psum_xaxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+psum_serial_run = acqavg(averaging_num, runacq_touchbyall, "psum.x", 1, probsize, True)
+psum_serial_time = psum_serial_run[0]
+
+psum_parallel_time = []
+psum_parallel_overh = []
+for proc in procnum_list:
+    procrun = acqavg(averaging_num, runacq_prefixsum, "psum.x", proc, probsize, False)
+    psum_parallel_time.append(procrun[1])
+    psum_parallel_overh.append(esf(float(psum_serial_time / procrun[1]), proc))
