3b6c2484ef02a068e27703bd941baee484c5c2db
Emanuele Ballarin <emanuele@ballarin.cc>
2019-12-30 04:43:41 +0100

    Useless update

---
+++ A03/mynbody.cpp
@@ -0,0 +1,22 @@
+/*──────────────────────────────────────────────────────────────────────────── *
+ *                                                                             *
+ * |> MYNBody - Move Your N-Body <|                                            *
+ *                                                                             *
+ * Just another un-physical N-body collisional gravitational simulation.       *
+ * Parallelized, though. At least that. ;)                                     *
+ *                                                                             *
+ * (Maybe) updated version and other cool stuff:                               *
+ * https://github.com/emaballarin/fhpc_assignments_2019-2020/tree/master/A03   *
+ *                                                                             *
+ *                                                                             *
+ * Copyright (C) 2019-* Emanuele Ballarin <emanuele@ballarin.cc>.              *
+ * All rights reserved. Distribution: Apache License 2.0.                      *
+ * Full text: https://ballarin.cc/legal/licenses/apacheii.txt                  *
+ *                                                                             *
+ *                                                                             *
+ * "Any fool can write code that a computer can understand. Good programmers   *
+ *  write code that humans can understand."                                    *
+ *                                               (M. Fowlers)                  *
+ *                                                                             *
+ * ─────────────────────────────────────────────────────────────────────────── */
+
